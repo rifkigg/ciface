b2b4cebf28064baf13a2cbc9027a54f2b5f6b083
rifkigg <rifkiadi348@gmail.com>
2025-10-19 11:34:27 +0700

    init

---
+++ src/app/components/Camera.js
@@ -0,0 +1,7 @@
+function Camera() {
+  return (
+    <div className="h-[450px] border-2">Camera</div>
+  )
+}
+
+export default Camera
---
+++ src/app/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="text-sm text-center italic">
+        <p>&copy; 2025 CiFace. Ramalan berdasarkan AI</p>
+    </footer>
+  )
+}
+
+export default Footer
